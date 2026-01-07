cec7f9523a8341c5d25e4a70a94758aeb92669aa
Akshasnh Kakkar <akshanshkakkar92@gmail.com>
2026-01-07 15:47:03 +0530

    final commit

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const cors = require('cors')
-const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+dotenv.config()
+
+const app = express();
 const tasks = require('./Routes/task')
 const PORT = process.env.PORT || 3000;
-dotenv.config()
 
 app.use(express.json())
 app.use(cors())
@@ -14,7 +15,7 @@ app.use('/api/tasks', tasks)
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         app.listen(PORT, () => {
-            console.log(`Server is Listening at http://localhost${PORT} and database is connected`)
+            console.log(`Server is Listening at http://localhost:${PORT} and database is connected`)
         })
     })
     .catch((error) => {
